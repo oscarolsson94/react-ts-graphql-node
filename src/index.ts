9df4c5245089799e60e56bade67bb218729cef4a
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-12-27 19:09:35 +0100

    setting up mikro-orm

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [Post],
-    dbName: "",
-    /* user: "root",
-    password: "root", */
-    type: "postgresql",
-    debug: !__prod__,
-  });
+  const orm = await MikroORM.init(microConfig);
 };
 
 main();
