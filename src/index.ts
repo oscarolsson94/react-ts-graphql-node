b885f4ece86428dbff1f9f76466baa21829d88c9
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-12-27 18:05:14 +0100

    creating first postgres entity

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
 
 const main = async () => {
   const orm = await MikroORM.init({
+    entities: [Post],
     dbName: "",
     /* user: "root",
     password: "root", */
