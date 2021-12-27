4e9ab8f89143c3013bab693f61e29ce9ea7a1029
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-12-27 19:23:40 +0100

    config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 
 const main = async () => {
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,12 +1,19 @@
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
+import path from "path";
 
 export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
   entities: [Post],
   dbName: "reddit",
-  /* user: "root",
-    password: "root", */
+  user: "root",
+  password: "root",
   type: "postgresql",
   debug: !__prod__,
 } as Parameters<typeof MikroORM.init>[0];
+
+https://youtu.be/I6ypD7qv3Z8?t=1930
