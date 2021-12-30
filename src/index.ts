6dae9e05808d225aad48f112cd1dee46c415bea4
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-12-30 20:05:59 +0100

    creating express server and starting apollo stuff

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+
+const PORT = 4000;
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up(); //runs migrations
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  app.listen(4000, () => {
+    console.log(`server started on localhost:4000 and port ${PORT}`);
+  });
 };
 
 main();
