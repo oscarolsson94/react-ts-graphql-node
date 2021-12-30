a2fa434a6b4c0916464593207176f4b7052686a8
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-12-30 20:20:12 +0100

    first graphql resolver to return all posts

--- dist/index.js
+++ dist/index.js
@@ -9,6 +9,7 @@ const express_1 = __importDefault(require("express"));
 const apollo_server_express_1 = require("apollo-server-express");
 const type_graphql_1 = require("type-graphql");
 const hello_1 = require("./resolvers/hello");
+const post_1 = require("./resolvers/post");
 const PORT = 4000;
 const main = async () => {
     const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
@@ -16,9 +17,10 @@ const main = async () => {
     const app = (0, express_1.default)();
     const apolloServer = new apollo_server_express_1.ApolloServer({
         schema: await (0, type_graphql_1.buildSchema)({
-            resolvers: [hello_1.HelloResolver],
+            resolvers: [hello_1.HelloResolver, post_1.PostResolver],
             validate: false,
         }),
+        context: () => ({ em: orm.em }),
     });
     apolloServer.applyMiddleware({ app });
     app.listen(4000, () => {
